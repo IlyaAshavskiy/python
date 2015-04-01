a164a59c6ccfbdc8762dca83564b4b259bae8657
IlyaAshavskiy <iashavskiy@gmail.com>
2015-04-01 23:41:15 +0300

    commit all

--- func_unpack.py
+++ func_unpack.py
@@ -52,27 +52,44 @@ def walk(dir):
                 output = filename.read(int(howmany))
                 output = struct.unpack(str(form), output)
                 return output
-            about = unpacker(0, 4, '4s')
+            about = unpacker(0, 4, '>4s')
             if about == ('QFI\xfb',):
                 backing_file_offset = unpacker(8, 8, '>Q')
                 virtual_size = unpacker(24, 8, '>Q')
                 nb_snapshots = unpacker(60, 4, '>I')
-                info_update = {"filename": path,"size": None, "virtual_size": virtual_size}
+                information = {
+                    "filename": path, "size": None, "virtual_size": virtual_size}
                 with open('info.json', 'w') as outfile:
                     json.dump(information, outfile)
                 # test if we have backing file then add name of it to our json
                 if backing_file_offset != (0,):
                     backing_file_size = unpacker(16, 4, '>I')
-                    backing_file_info = unpacker(backing_file_offset[0], backing_file_size[0], str(backing_file_size[0]) + 's')
+                    backing_file_info = unpacker(backing_file_offset[0], backing_file_size[
+                                                 0], '>' + str(backing_file_size[0]) + 's')
                     information.setdefault("backing_file", backing_file_info)
                 if nb_snapshots != (0,):
                     snapshots_offset = unpacker(64, 8, '>Q')
-                    snapshot_id=unpacker(snapshots_offset,)
-                    information.setdefault()
+                    print('snapshots_offset', snapshots_offset)
+                    snapint = snapshots_offset[0] + 56
+                    snapshot_id_len = unpacker(
+                        snapshots_offset[0] + 12, 2, '>H')
+                    snapshot_id = unpacker(
+                        snapint, snapshot_id_len[0], '>' + str(snapshot_id_len[0]) + 's')
+                    snapshot_name_len = unpacker(
+                        snapshots_offset[0] + 14, 2, '>H')
+                    snapshot_name = unpacker(
+                        snapint + snapshot_id_len[0], snapshot_name_len[0], '>' + str(snapshot_name_len[0]) + 's')
+                    snapshot_size = unpacker(snapshots_offset[0] + 48, 8, '>Q')
+                    # snapshot_size=unpacker(snapint,snapshot_size_len[0],'>'+str(snapshot_size_len[0])+'s')
+                    gotonextsnap =snapshot_size+1
+                    print('snapshot_id', snapshot_id[0])
+                    print('snapshot_name', snapshot_name[0])
+                    print('snapshot_size', snapshot_size[0])
+                    # information.setdefault()
 
             filename.close()
 
         else:
             walk(path)
 
-walk('/home/ilya/TESTQCOW/')
+walk('/home/ilya/TESTQCOW/QCOW')
